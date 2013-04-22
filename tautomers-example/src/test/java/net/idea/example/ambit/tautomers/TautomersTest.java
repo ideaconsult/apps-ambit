e8e7316031c26bed24e699dd028a1b462e9adb56
ngn <jeliazkova.nina@gmail.com>
2013-04-22 12:32:38 +0300

    Added test and a test file with InChIs

---
+++ tautomers-example/src/test/java/net/idea/example/ambit/tautomers/TautomersTest.java
@@ -0,0 +1,18 @@
+package net.idea.example.ambit.tautomers;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+public class TautomersTest {
+
+	@Test
+	public void test() throws Exception {
+		MainApp app = new MainApp();
+		URL url = TautomersTest.class.getClassLoader().getResource("net/idea/example/ambit/tautomers/inchi.txt");
+		org.junit.Assert.assertNotNull(url.getFile());
+		String[] args = new String[] {"-"+MainApp._option.file.getShortName(),url.getFile()};
+		for (String arg:args) System.out.println(arg);
+		app.run(args);
+	}
+}
