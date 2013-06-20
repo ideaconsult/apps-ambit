b373b1a13f8d5ebc832dd4b479f291f553ba801d
ngn <jeliazkova.nina@gmail.com>
2013-06-20 14:36:54 +0300

    added test with a file where the processing stops after #5

--- tautomers-example/src/test/java/net/idea/example/ambit/tautomers/TautomersTest.java
+++ tautomers-example/src/test/java/net/idea/example/ambit/tautomers/TautomersTest.java
@@ -31,4 +31,17 @@ public class TautomersTest {
 		for (String arg:args) System.out.println(arg);
 		app.run(args);
 	}
+	
+	@Test
+	public void testDrugBankError() throws Exception {
+		MainApp app = new MainApp();
+		URL url = TautomersTest.class.getClassLoader().getResource("net/idea/example/ambit/tautomers/drugbank_error.sdf");
+		
+		org.junit.Assert.assertNotNull(url.getFile());
+		String[] args = new String[] {
+				"-"+MainApp._option.file.getShortName(),url.getFile(),
+				"-"+MainApp._option.output.getShortName(),url.getFile().replace(".sdf", "_result.sdf")};
+		for (String arg:args) System.out.println(arg);
+		app.run(args);
+	}
 }
