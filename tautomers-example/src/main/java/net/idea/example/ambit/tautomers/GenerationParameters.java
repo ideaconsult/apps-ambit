9b1f0e9296fc53ffea1dd6b7967cf20cb5a40823
ntk73 <nick@uni-plovdiv.net>
2014-11-16 10:13:05 +0200

    Added class GenerationParameters

---
+++ tautomers-example/src/main/java/net/idea/example/ambit/tautomers/GenerationParameters.java
@@ -0,0 +1,11 @@
+package net.idea.example.ambit.tautomers;
+
+public class GenerationParameters 
+{
+	public static GenerationParameters parseFromString(String s)
+	{
+		GenerationParameters gen = new GenerationParameters();
+		//TODO
+		return gen;
+	}
+}
