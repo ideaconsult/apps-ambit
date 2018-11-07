6808108400d79fa6691a387f415542baac98f608
ntk73 <nick@uni-plovdiv.net>
2018-11-07 15:39:06 +0200

    Implemented input file iteration and zwitterion generation

--- tautomers-example/src/main/java/net/idea/example/ambit/tautomers/ZwitterionCli.java
+++ tautomers-example/src/main/java/net/idea/example/ambit/tautomers/ZwitterionCli.java
@@ -1,5 +1,10 @@
 package net.idea.example.ambit.tautomers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.InputStream;
 import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
@@ -9,9 +14,17 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.PosixParser;
+import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.IChemObjectReaderErrorHandler;
+import org.openscience.cdk.io.IChemObjectReader.Mode;
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
+import ambit2.base.exceptions.AmbitIOException;
+import ambit2.core.io.FileInputState;
+import ambit2.core.io.InteractiveIteratingMDLReader;
 import ambit2.smarts.SmartsHelper;
 import ambit2.tautomers.zwitterion.ZwitterionManager;
 
@@ -22,6 +35,10 @@ public class ZwitterionCli
 	public String inputFileName = null;
 	public String outputFileName = null;
 	public String inputSmiles = null;
+	
+	FileWriter outWriter = null;	
+	ZwitterionManager zwittMan = null;
+	String endLine = System.getProperty("line.separator");
 
 	public static void main(String[] args) {
 		ZwitterionCli zwitt = new ZwitterionCli();
@@ -68,7 +85,7 @@ public class ZwitterionCli
 			}
 			@Override
 			public String getDescription() {
-				return "Input molecule file";
+				return "Input molecule file (*.smi, *.sdf)";
 			}
 			@Override
 			public String getShortName() {
@@ -201,7 +218,7 @@ public class ZwitterionCli
 		if ((inputFileName == null) && (inputSmiles == null))
 		{
 			System.out.println("No input is given! \n"
-					+ "Please assign input SMILES or input molecules file)!");
+					+ "Please assign input SMILES or input molecules file!");
 			System.out.println("Use option '-h' for help.");
 			return -1;
 		}
@@ -226,7 +243,7 @@ public class ZwitterionCli
 
 			System.out.println("Input molecule: " + inputSmiles);
 
-			ZwitterionManager zwittMan = new ZwitterionManager();
+			zwittMan = new ZwitterionManager();
 			zwittMan.setStructure(mol);
 
 			try {
@@ -249,10 +266,157 @@ public class ZwitterionCli
 			return 0;
 		}
 
-		//Handle input file
-		//TODO
+		
+		//Create output file		
+		if (outputFileName != null)
+		{	
+			try {
+				outWriter = new FileWriter(outputFileName);
+			}
+			catch (Exception x) {
+				System.out.println("Unable to create output file: " + outputFileName 
+						+ " error: " + x.getMessage());
+				return -5;
+			}
+		}
+		
+		//Iterate input file and generate zwitterions 
+		zwittMan = new ZwitterionManager();		
+		File file = new File (inputFileName);
+		try {
+			iterateInputFile(file);
+		}
+		catch (Exception x) {
+			System.out.println("Error on iterating input file: " + outputFileName
+					+ ": " + x.getMessage());
+		}
+		
+		//Close output file	
+		try {
+			if (outWriter != null)
+				outWriter.close();
+		}
+		catch (Exception x) {
+			System.out.println("Error on closing output file: " + outputFileName
+					+ ": " + x.getMessage());
+		}
 
 		return 0;
+	}
+	
+	void processMolecule(IAtomContainer mol, int recordNum) throws Exception
+	{	
+		zwittMan.setStructure(mol);
+		List<IAtomContainer> zwList = zwittMan.generateZwitterions();
+		
+		String out_s = ""+ recordNum + "," + SmartsHelper.moleculeToSMILES(mol, true);
+		outputLine(out_s);
+		
+		for (int i = 0; i < zwList.size(); i++)
+		{	
+			out_s = ""+ recordNum + "," + SmartsHelper.moleculeToSMILES(zwList.get(i), true);
+			outputLine(out_s);
+		}
+	}
+	
+	void outputLine(String line) throws Exception
+	{
+		if (outWriter == null)
+			System.out.println(line);
+		else
+		{	
+			outWriter.write(line);
+			outWriter.write(endLine);
+			outWriter.flush();
+		}
+	}
+	
+	public void iterateInputFile(File file) throws Exception
+	{
+		int records_read = 0;
+		int records_processed = 0;
+		int records_error = 0;
+		
+		if (!file.exists()) 
+			throw new FileNotFoundException(file.getAbsolutePath());
+		
+		InputStream in = new FileInputStream(file);
+		IIteratingChemObjectReader<IAtomContainer> reader = null;
+		try 
+		{
+			System.out.println("Generating zwitterions for: " + file.getPath());
+			
+			String out_s = "Mol#,Zwitterion";
+			outputLine(out_s);
+			
+			reader = getReader(in,file.getName());
+			while (reader.hasNext()) 
+			{
+				IAtomContainer molecule  = reader.next();
+				records_read++;
+				if (molecule==null) {
+					records_error++;
+					System.out.println("Unable to read molecule from record #" + records_read);
+					continue;
+				}
+				
+				try
+				{	
+					processMolecule(molecule, records_read);
+					records_processed++;
+				}
+				catch (Exception x)
+				{
+					System.out.println("Error on record #" + records_read + " " + x.getMessage());
+				}
+			}
+			
+			System.out.println("Done!");
+			System.out.println("records_read: " + records_read 
+					+ ", records_processed: " + records_processed 
+					+ ", records_error: " + records_error);
+		}
+		catch (Exception x1) {
+			//logger.log(Level.SEVERE, String.format("[Record %d] Error %s\n", records_read, file.getAbsoluteFile()), x1);
+		} 
+		finally {
+			try { reader.close(); } catch (Exception x) {}
+		}
+	}
+	
+	protected IIteratingChemObjectReader<IAtomContainer> getReader(InputStream in, String extension) throws CDKException, AmbitIOException {
+		FileInputState instate = new FileInputState();
+		IIteratingChemObjectReader<IAtomContainer> reader ;
+		if (extension.endsWith(FileInputState._FILE_TYPE.SDF_INDEX.getExtension())) {
+			reader = new InteractiveIteratingMDLReader(in,SilentChemObjectBuilder.getInstance());
+			((InteractiveIteratingMDLReader) reader).setSkip(true);
+		} else reader = instate.getReader(in,extension);
+		
+		reader.setReaderMode(Mode.RELAXED);
+		reader.setErrorHandler(new IChemObjectReaderErrorHandler() {
+			
+			@Override
+			public void handleError(String message, int row, int colStart, int colEnd,
+					Exception exception) {
+				exception.printStackTrace();
+			}
+			
+			@Override
+			public void handleError(String message, int row, int colStart, int colEnd) {
+				System.out.println(message);
+			}
+			
+			@Override
+			public void handleError(String message, Exception exception) {
+				exception.printStackTrace();				
+			}
+			
+			@Override
+			public void handleError(String message) {
+				System.out.println(message);
+			}
+		});
+		return reader;
 	}
 
 }
